feef80c338191e7f5b727736b71ac23a8cafcaef
Michael Avila <me@michaelavila.com>
2015-12-24 16:20:11 -0800

    Run rcup if user has dotfiles

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,15 @@ include homebrew
 
 class dotfiles {
   homebrew::tap { 'thoughtbot/formulae': }
+
   package { 'rcm':
     ensure => present
+  }
+
+  $home = "/Users/${::boxen_user}"
+
+  exec { 'rcup':
+    command => 'rcup',
+    require => File["${home}/.dotfiles"],
   }
 }
